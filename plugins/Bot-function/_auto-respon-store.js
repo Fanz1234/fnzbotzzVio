a67f36c023dddf2d1f1402bb1f18c3faa918dc31
Fanz1234 <118971536+Fanz1234@users.noreply.github.com>
2024-06-09 13:17:00 +0700

    Add files via upload

--- plugins/Bot-function/_auto-respon-store.js
+++ plugins/Bot-function/_auto-respon-store.js
@@ -7,12 +7,21 @@ handler.before = async function (m, { conn }) {
   //Respon listStore
   let store = m.isGroup ? global.db.data.chats[m.chat].store : false;
   if (store) {
-    let listStore = global.db.data.chats[m.chat].store[m.body.toLowerCase()];
+    let listStore = global.db.data.chats[m.chat].store[m.body];
     if (listStore) {
       if (_spam.check("NotCase", m.senderNumber, AntiSpam)) return;
       _spam.add("NotCase", m.senderNumber, "10s", AntiSpam);
-      m.reply(listStore.text);
+
+      if(listStore.hasOwnProperty('text')){
+        m.reply(listStore.text);
+      } else conn.sendFile(m.chat, listStore.media, m.body, listStore.caption, m, false, {})
+
+
+      
     }
   }
 };
 export default handler;
+
+
+
