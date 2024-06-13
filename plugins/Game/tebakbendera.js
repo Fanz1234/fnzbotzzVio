4f9795a1e2ebda1107f2b5ecdcec0cf64431bbd1
Fanz1234 <118971536+Fanz1234@users.noreply.github.com>
2024-06-14 06:47:45 +0700

    Add files via upload

--- plugins/Game/tebakbendera.js
+++ plugins/Game/tebakbendera.js
@@ -1,42 +1,74 @@
-import fs from 'fs-extra'
-let timeout = 120000;
-let poin = 4999;
-let handler = async (m, { conn, command, usedPrefix }) => {
-  conn.game = conn.game ? conn.game : {};
-  let id = "tebakbendera-" + m.chat;
-  if (id in conn.game)
-    return conn.reply(
-      m.chat,
-      "Masih ada soal belum terjawab di chat ini",
-      conn.game[id][0]
-    );
-  let src = JSON.parse(fs.readFileSync("./lib/game/tebakbendera.json", "utf-8"));
-  let json = src[Math.floor(Math.random() * src.length)];
-  let caption = `
-bendera apakah ini?
+/*let fetch = require('node-fetch')
 
+let timeout = 180000
+let poin = 500
+let handler = async (m, { conn, usedPrefix }) => {
+  conn.tebakbendera = conn.tebakbendera ? conn.tebakbendera : {}
+  let id = m.chat
+  if (id in conn.tebakbendera) {
+    conn.reply(m.chat, 'Masih ada soal belum terjawab di chat ini', conn.tebakbendera[id][0])
+    throw false
+  }
+  let src = await (await fetch('https://raw.githubusercontent.com/BochilTeam/database/master/games/tebakbendera.json')).json()
+    let json = src[Math.floor(Math.random() * src.length)]
+  // if (!json.status) throw json
+  let caption = `
 Timeout *${(timeout / 1000).toFixed(2)} detik*
-Ketik ${usedPrefix}hbend untuk bantuan
+Ketik ${usedPrefix}tekbe untuk hint
 Bonus: ${poin} XP
-`.trim();
-  conn.game[id] = [
-    await conn.sendFile(m.chat, json.img, "tebakbendera.jpg", caption, m),
-    json,
-    poin,
+    `.trim()
+  conn.tebakbendera[id] = [
+    await conn.sendFile(m.chat, json.img, 'tebakbendera.jpg', caption, m, false, { thumbnail: Buffer.alloc(0) }),
+    json, poin,
     setTimeout(() => {
-      if (conn.game[id])
-        conn.reply(
-          m.chat,
-          `Waktu habis!\nJawabannya adalah *${json.jawaban}*`,
-          conn.game[id][0]
-        );
-      delete conn.game[id];
-    }, timeout),
-  ];
-};
-handler.help = ["tebakbendera"];
-handler.tags = ["game"];
-handler.command = /^tebakbendera$/i;
+      if (conn.tebakbendera[id]) conn.reply(m.chat, `Waktu habis!\nJawabannya adalah *${json.name}*`, conn.tebakbendera[id][0])
+      delete conn.tebakbendera[id]
+    }, timeout)
+  ]
+}
+handler.help = ['tebakbendera (error)']
+handler.tags = ['game']
+handler.command = /^tebakbendera/i
+handler.limit = true
+handler.group = true
+
+module.exports = handler*/
+
+
+let fetch = require('node-fetch')
+
+let timeout = 120000
+let poin = 1000
+let tiketcoin = 1
+let src
+let handler = async (m, { conn, usedPrefix }) => {
+    conn.tebakbendera = conn.tebakbendera ? conn.tebakbendera : {}
+    let id = m.chat
+    if (id in conn.tebakbendera) {
+        conn.reply(m.chat, 'Masih ada soal belum terjawab di chat ini', conn.tebakbendera[id][0])
+        throw false
+    }
+    if (!src) src = await (await fetch(global.API('https://raw.githubusercontent.com', '/qisyana/scrape/main/flag.json'))).json()
+    let json = src[Math.floor(Math.random() * src.length)]
+    if (!json) throw json
+    let caption = `
+Timeout *${(timeout / 1000).toFixed(2)} detik*
+Ketik ${usedPrefix}tekbe untuk bantuan
+Bonus: ${poin} XP
+Tiketcoin: ${tiketcoin} TiketCoin
+`.trim()
+    conn.tebakbendera[id] = [
+        await conn.sendFile(m.chat, json.img, 'img.jpg', caption, m),
+        json, poin,
+        setTimeout(() => {
+            if (conn.tebakbendera[id]) conn.reply(m.chat, `Waktu habis!\nJawabannya adalah *${json.name}*`, conn.tebakbendera[id][0])
+            delete conn.tebakbendera[id]
+        }, timeout)
+    ]
+}
+handler.help = ['tebakbendera'];
+handler.tags = ['game'];
+handler.command = /^tebakbendera/i;
 
 handler.onlyprem = true;
 handler.game = true;
