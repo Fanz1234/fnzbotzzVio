4913a8604e062a3bdad6b174cc73823360524f4f
Fanz1234 <118971536+Fanz1234@users.noreply.github.com>
2024-06-16 14:13:25 +0700

    Add files via upload

--- plugins/AI/toanime2.js
+++ plugins/AI/toanime2.js
@@ -1,7 +1,6 @@
 import fetch from "node-fetch";
 import { TelegraPh } from "../../lib/uploader.js";
-import { miftah, nazmy, nekohime } from '../../lib/restApi.js';
-
+import { miftah,nazmy,nekohime } from '../../lib/restApi.js'
 let handler = async (m, { conn, usedPrefix, command, text }) => {
   let who =
     m.mentionedJid && m.mentionedJid[0]
@@ -12,39 +11,28 @@ let handler = async (m, { conn, usedPrefix, command, text }) => {
   let name = await conn.getName(who);
   let q = m.quoted ? m.quoted : m;
   let mime = (q.msg || q).mimetype || "";
-  
-  if (!mime) return m.reply("Kirim/Reply Gambar Dengan Caption .toanime");
-
+  if (!mime) return m.reply("Kirim/Reply Gambar Dengan Caption .toanime")
   m.reply(mess.wait);
-  
   try {
     let media = await conn.downloadAndSaveMediaMessage(q, makeid(5));
     let url = await TelegraPh(media);
-    
-    // Fetching the image from the specified API endpoint
-    let response = await fetch(`https://aemt.me/toanime?url=${url}`);
-    let data = await response.json();
-    
-    // Assuming the API returns a direct image URL
-    let imageUrl = data.image; // Adjust this based on the actual JSON response structure
-    
-    // Sending the image URL as a message
-    await conn.sendMessage(
+    let hasil = `https://aemt.me/toanime?url=${url}`;
+    await conn.sendMessageWithDownload(
       m.chat,
       {
-        image: { url: imageUrl },
+        image: { url: hasil },
         caption: "Nih Kak, Maaf Kalau Hasilnya Tidak Sesuai Keinginan",
       },
       { quoted: m }
     );
-    
   } catch (e) {
     m.reply(`${e}`);
   }
 };
-
 handler.help = ["toanime2"];
 handler.tags = ["ai"];
 handler.command = /^(jadianime2|toanime2)$/i;
+handler.premium = false;
+handler.limit = true;
 
 export default handler;
