59de1c7246a35ba1e4f9d81df60f1c73e0764c6e
Fanz1234 <118971536+Fanz1234@users.noreply.github.com>
2024-06-16 23:33:49 +0700

    Add files via upload

---
+++ plugins/Case/toanime2.js
@@ -0,0 +1,44 @@
+import fetch from "node-fetch";
+import { TelegraPh } from "../../lib/uploader.js";
+import { miftah,nazmy,nekohime } from '../../lib/restApi.js'
+let handler = async (m, { conn, usedPrefix, command, text }) => {
+  let who =
+    m.mentionedJid && m.mentionedJid[0]
+      ? m.mentionedJid[0]
+      : m.fromMe
+      ? conn.user.jid
+      : m.sender;
+  let name = await conn.getName(who);
+  let q = m.quoted ? m.quoted : m;
+  let mime = (q.msg || q).mimetype || "";
+  if (!mime) return m.reply("Kirim/Reply Gambar Dengan Caption .toanime")
+  m.reply(mess.wait);
+  try {
+    let media = await conn.downloadAndSaveMediaMessage(q, makeid(5));
+    let url = await TelegraPh(media);
+    let hasil = `https://aemt.me/toanime?url=${url}`;
+   
+ // Download the URL content
+let response = await fetch(hasil);
+let downloadedContent = await response.url();
+
+// Send the downloaded URL content
+await conn.sendMessage(
+      m.chat, downloadedContent,
+      {
+        image: { url: hasil },
+        caption: "Nih Kak, Maaf Kalau Hasilnya Tidak Sesuai Keinginan",
+      },
+      { quoted: m }
+    );
+  } catch (e) {
+    m.reply(`${e}`);
+  }
+};
+handler.help = ["toanime2"];
+handler.tags = ["ai"];
+handler.command = /^(jadianime2|toanime2)$/i;
+handler.premium = false;
+handler.limit = true;
+
+export default handler;
