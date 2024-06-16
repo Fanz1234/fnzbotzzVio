3d58c4d4812419f7778aab5f5ad34880f386454e
Fanz1234 <118971536+Fanz1234@users.noreply.github.com>
2024-06-16 21:56:39 +0700

    Add files via upload

--- plugins/AI/toanime2.js
+++ plugins/AI/toanime2.js
@@ -1,10 +1,20 @@
 import fetch from "node-fetch";
 import { TelegraPh } from "../../lib/uploader.js";
-let handler = async (m, { conn, text }) => {
+import { miftah,nazmy,nekohime } from '../../lib/restApi.js'
+let handler = async (m, { conn, usedPrefix, command, text }) => {
+  let who =
+    m.mentionedJid && m.mentionedJid[0]
+      ? m.mentionedJid[0]
+      : m.fromMe
+      ? conn.user.jid
+      : m.sender;
+  let name = await conn.getName(who);
+  let q = m.quoted ? m.quoted : m;
   let mime = (q.msg || q).mimetype || "";
   if (!mime) return m.reply("Kirim/Reply Gambar Dengan Caption .toanime")
   m.reply(mess.wait);
-    let media = await conn.downloadAndSaveMediaMessage(q, makeid(5));
+  try {
+    let media = await conn.downloadAndSaveMediaMessage(quoted);
     let url = await TelegraPh(media);
     let hasil = `https://aemt.me/toanime?url=${url}`;
     await conn.sendMessage(
@@ -22,7 +32,5 @@ let handler = async (m, { conn, text }) => {
 handler.help = ["toanime2"];
 handler.tags = ["ai"];
 handler.command = /^(jadianime2|toanime2)$/i;
-handler.premium = false;
-handler.limit = true;
-
+ 
 export default handler;
