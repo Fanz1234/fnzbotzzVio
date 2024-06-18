bbb06df4a123e6cdfe6f15ad63b218b8f7346134
Fanz1234 <118971536+Fanz1234@users.noreply.github.com>
2024-06-18 14:31:38 +0700

    Add files via upload

---
+++ plugins/Others/sshp.js
@@ -0,0 +1,11 @@
+import fetch from 'node-fetch'
+  import axios from 'axios'
+let handler = async(m, { conn, usedPrefix, command, args }) => {
+  let res = await(await fetch(`https://aemt.me/sshp?url=${args[0]}`)).buffer()
+  if(!args[0]) throw `Linknya mana?`
+conn.sendHydrated(m.chat, `*「 Screenshot Web 」*\n\n*💻 Url:* ${args[0]}`, wm, res, `${args[0]}`, '🌎 U R L', null, null, [[null,null],[null,null],[null,null]], m)
+}
+handler.help = ['sshp']
+handler.tags = ['internet']
+handler.command = /^sshp$/i
+export default handler
