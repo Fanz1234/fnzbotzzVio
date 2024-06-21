fb759bbf68406418026922d383260cd25e07dd85
Fanz1234 <118971536+Fanz1234@users.noreply.github.com>
2024-06-21 18:54:03 +0700

    Add files via upload

--- plugins/Case/case.js
+++ plugins/Case/case.js
@@ -331,6 +331,276 @@ case 'fnzaiimg4': {
   }
   break
   
+  // CEK KHODAM TYPE SUARA 
+// BY LANA
+
+case  'cekkhodam2':{
+    const defaultLang = 'id';
+    const gtts = require( 'node-gtts')
+    // Penanganan input nama
+    let lana;
+    if (args.length === 0 || !args[0]) {
+        return m.reply('Harap masukkan nama kamu.');
+    } else {
+        lana = args[0];
+    }
+
+    let lang = args[1];
+    if (!lang || lang.length !== 2) {
+        lang = defaultLang;
+    }
+    
+    const khodam = [
+  "Singa",
+  "kosoong atau tidak ada",
+  "Harimau",
+  "Elang",
+  "Serigala",
+  "Naga",
+  "Gajah",
+  "Kuda",
+  "Macan Tutul",
+  "Kerbau",
+  "Burung Hantu",
+  "Burung Rajawali",
+  "Ikan Hiu",
+  "Lumba-Lumba",
+  "Ular",
+  "Kura-Kura",
+  "Tupai",
+  "Paus",
+  "Kelelawar",
+  "Kijang",
+  "Banteng",
+  "Rusa",
+  "Anjing",
+  "Kucing",
+  "Buaya",
+  "Kambing",
+  "Kuda Nil",
+  "Bebek",
+  "Angsa",
+  "Ayam",
+  "Merpati",
+  "Burung Beo",
+  "Burung Kenari",
+  "Burung Kakatua",
+  "Bunglon",
+  "Cicak",
+  "Kodok",
+  "Katak",
+  "Tikus",
+  "Landak",
+  "Kanguru",
+  "Koala",
+  "Panda",
+  "Beruang",
+  "Rubah",
+  "Lynx",
+  "Leopard",
+  "Jaguar",
+  "Cheetah",
+  "Badak",
+  "Zebra",
+  "Antelop",
+  "Unta",
+  "Alpaka",
+  "Llama",
+  "Serigala Abu-abu",
+  "Serigala Merah",
+  "Serigala Putih",
+  "Lynx",
+  "Harimau Putih",
+  "Harimau Siberia",
+  "Harimau Sumatra",
+  "Gorila",
+  "Orangutan",
+  "Simpanse",
+  "Monyet",
+  "Babun",
+  "Lemur",
+  "Iguana",
+  "Komodo",
+  "Salamander",
+  "Belut",
+  "Sotong",
+  "Gurita",
+  "Kepiting",
+  "Lobster",
+  "Udang",
+  "Kupu-kupu",
+  "Lebah",
+  "Tawon",
+  "Kumbang",
+  "Belalang",
+  "Jangkrik",
+  "Semut",
+  "Kecoak",
+  "Laba-laba",
+  "Kalajengking",
+  "Serangga Tongkat",
+  "Naga Laut",
+  "Kuda Laut",
+  "Duyung",
+  "Putri Duyung",
+  "Burung Kolibri",
+  "Burung Hantu Salju",
+  "Burung Puyuh",
+  "Burung Gagak",
+  "Burung Pelikan",
+  "Burung Albatros",
+  "Burung Flamingo",
+  "Burung Hering",
+  "Burung Camar",
+  "Burung Pinguin",
+    "Cincin",
+    "Batu Akik",
+    "Keris",
+    "Tongkat",
+    "Pusaka",
+    "Patung",
+    "Mustika",
+    "Tasbih",
+    "Kalung",
+    "Gelang",
+    "Permata",
+    "Pedang",
+    "Mata Uang",
+    "Wesi Kuning",
+    "Serat Tali",
+    "Belati",
+    "Cundrik",
+    "Selendang",
+    "Jarum",
+    "Tombak",
+    "Kerikil",
+    "Kendi",
+    "Kain Kafan",
+    "Topi",
+    "Payung",
+    "Sandal",
+    "Kacamata",
+    "Sabuk",
+    "Sarung",
+    "Tali Ikat Pinggang",
+    "Surat",
+    "Kunci",
+    "Lilin",
+    "Peniti",
+    "Sisir",
+    "Cermin",
+    "Kendi Air",
+    "Piring",
+    "Gelas",
+    "Mangkuk",
+    "Sendok",
+    "Sapu",
+    "Gayung",
+    "Tikar",
+    "Bantal",
+    "Guci",
+    "Lentera",
+    "Lampu",
+    "Buku",
+    "Pena",
+    "Dupa",
+    "Asbak",
+    "Cangkir",
+    "Gantungan Kunci",
+    "Kalender",
+    "Sepeda",
+    "Lukisan",
+    "Batu Kerikil",
+    "Batu Kali",
+    "Kipas",
+    "Peci",
+    "Sorban",
+    "Rokok",
+    "Topeng",
+    "Gamelan",
+    "Angklung",
+    "Suling",
+    "Wayang",
+    "Kuda-Kudaan",
+    "Sepatu",
+    "Jam Tangan",
+    "Kosong atau tidak adaa"
+];
+
+
+    const randomKhodam = khodam[Math.floor(Math.random() * khodam.length)];
+    let text = `Khodam ${lana} adalah ${randomKhodam}`;
+function tts(text, lang = 'id') {
+return new Promise((resolve, reject) => {
+try {
+let tts = gtts(lang)
+let filePath =  (1 * new Date) + '.mp3'
+tts.save(filePath, text, () => {
+resolve(fs.readFileSync(filePath))
+fs.unlinkSync(filePath)
+})
+} catch (e) { reject(e) }
+})
+}    
+    
+
+
+    let res;
+    try {
+        res = await tts(text, lang);
+    } catch (e) {
+        m.reply(e + '');
+        res = await tts(text, defaultLang);
+    } finally {
+        reply(text)
+        if (res) {
+await conn.sendMessage(m.chat, { audio: res, ptt: true, mimetype: "audio/mpeg", fileName: "vn.mp3", waveform: [100, 0, 100, 0, 100, 0, 100] }, { quoted: m });
+        }
+    }
+};
+break
+
+case 'spotify2':{
+  if (!q) retrun m.reply(`Judulnya apa, ya?`)
+  try {
+    await m.reply("`Tunggu Sebentar.....`");
+    const search = await fetch(
+      `https://api.maher-zubair.tech/search/spotify?q=${q}`,
+    );
+    const results = await search.json();
+    let { title, duration, popularity, artist, url } = results.result[0];
+
+    const lagu = await fetch(
+      `https://api.maher-zubair.tech/download/spotify?url=${url}`,
+    );
+    const reslagu = await lagu.json();
+    let { song, album_name, release_date, cover_url } = reslagu.result;
+    let teks = `     *[ Spotify ]*
+> *Judul:* ${title} 
+> *Populeritas:* ${popularity}
+> *Durasi:* ${duration}
+> *Artis:* ${artist}
+> *Album:* ${album_name}
+> *Rilis:* ${release_date}
+
+_Tunggu, sedang mengirim audio..._
+    `;
+    let msg = await conn.sendFile(m.chat, cover_url, "cover.jpg", teks, m);
+    await conn.sendMessage(
+      m.chat,
+      {
+        audio: { url: reslagu.result.url },
+        mimetype: "audio/mpeg",
+        fileName: title,
+      },
+      { quoted: m },
+    );
+  } catch (e) {
+    m.reply(e);
+  }
+}
+break
+  
   /*/case  'cekkhodam':{
 const tanggal = new Date().toLocaleDateString('id', { weekday: 'long' }) + ',' + ' ' + new Date().toLocaleDateString("id", {day: 'numeric', month: 'long', year: 'numeric'})
 const timeout = 86400000
