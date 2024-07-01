ff6ba31479eca196250b6596b713a8e64259aa2d
Fanz1234 <118971536+Fanz1234@users.noreply.github.com>
2024-07-01 14:08:18 +0700

    Add files via upload

--- plugins/Game/tebakanime.js
+++ plugins/Game/tebakanime.js
@@ -1,50 +1,37 @@
-import fetch from 'node-fetch';
+let fetch = require('node-fetch')
 
-let timeout = 120000;
-let poin = 500;
-
-let handler = async (m, { conn, usedPrefix, command }) => {
-    conn.tebakanime = conn.tebakanime || {}; // Initialize if not already initialized
-    let id = m.chat;
-
-    if (conn.tebakanime[id]) {
-        // If there's an active game for this chat ID
-        conn.reply(m.chat, 'Masih Ada Soal Yang Belum Terjawab', conn.tebakanime[id][0]);
-        throw false; // Exit the handler function
-    }
-
-    try {
-        let response = await fetch('https://raw.githubusercontent.com/unx21/ngetezz/main/src/data/nyenyenye.json');
-        let src = await response.json();
-        let json = src[Math.floor(Math.random() * src.length)];
-
-        let caption = `
-Waktu *${(timeout / 1000).toFixed(2)} Detik*
-Ketik ${usedPrefix}wa Untuk Bantuan
+let timeout = 180000
+let poin = 1000
+let tiketcoin = 1
+let handler = async (m, { conn, usedPrefix }) => {
+  conn.tebakgame = conn.tebakgame ? conn.tebakgame : {}
+  let id = m.chat
+  if (id in conn.tebakgame) {
+    conn.reply(m.chat, 'Masih ada soal belum terjawab di chat ini!', conn.tebakgame[id][0])
+    throw false
+  }
+  let src = await (await fetch('https://raw.githubusercontent.com/unx21/ngetezz/main/src/data/nyenyenye.json')).json()
+    let json = src[Math.floor(Math.random() * src.length)]
+  // if (!json.status) throw json
+  let caption = `
+Timeout *${(timeout / 1000).toFixed(2)} detik*
+Ketik ${usedPrefix}wa untuk clue
 Bonus: ${poin} XP
-`.trim();
+TiketCoin: ${tiketcoin}
+    `.trim()
+  conn.tebakgame[id] = [
+    await conn.sendFile(m.chat, json.img, 'tebakgame.jpg', caption, m, false, { thumbnail: Buffer.alloc(0) }),
+    json, poin,
+    setTimeout(() => {
+      if (conn.tebakgame[id]) conn.reply(m.chat, `Waktu habis!\nJawabannya adalah *${json.jawaban}*`, conn.tebakgame[id][0])
+      delete conn.tebakgame[id]
+    }, timeout)
+  ]
+}
+handler.help = ['tebakanime']
+handler.tags = ['game']
+handler.command = /^tebakanime/i
+handler.limit = true
+handler.group = false
 
-        let message = await conn.reply(m.chat, json.img, 'anu.jpg', caption, m, false);
-
-        conn.tebakanime[id] = [
-            message,
-            json,
-            poin,
-            setTimeout(async () => {
-                if (conn.tebakanime[id]) {
-                    await conn.reply(m.chat, `Waktu Habis!\nJawabannya Adalah *${json.jawaban}*`, conn.tebakanime[id][0]);
-                    delete conn.tebakanime[id];
-                }
-            }, timeout)
-        ];
-    } catch (error) {
-        console.error('Error fetching or processing data:', error);
-        throw error; // Throw the error further to handle it
-    }
-};
-
-handler.help = ['tebakanime'];
-handler.tags = ['game'];
-handler.command = /^tebakanime$/i;
-
-export default handler;
+module.exports = handler
